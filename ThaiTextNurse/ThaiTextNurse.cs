dc64228e32eb8d279a4879220296371e2374189d
Phun Peeticharoenthum <phunphun484@gmail.com>
2024-10-04 02:21:14 +0700

    EDIT: Simplify ThaiTextNurse

--- ThaiTextNurse/ThaiTextNurse.cs
+++ ThaiTextNurse/ThaiTextNurse.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using Lexto;
+﻿using Lexto;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace PhEngine.ThaiTMP
 {
     [RequireComponent(typeof(TMP_Text)), ExecuteAlways]
     public class ThaiTextNurse : MonoBehaviour, ITextPreprocessor
     {
-        private static readonly Char SEPARATOR = '\u200B';
-        private static readonly Char NEWLINE = '\u000A';
-        private static readonly Char SPACE = '\u0020';
-        private static readonly Char SPECIAL_TAG = '\uFFF0';
-
-        private static readonly Char APPEND_NEWLINE = '\n';
-
         [SerializeField, HideInInspector] TMP_Text tmpText;
         [SerializeField] bool isCorrectGlyphs = true;
         [SerializeField] bool isTokenize;
-        [FormerlySerializedAs("separatorPrefix")] [SerializeField] string separator;
+        [SerializeField] string separator;
         [SerializeField, HideInInspector] string lastKnownText;
         [SerializeField, HideInInspector] string lastProcessedText;
 
@@ -93,10 +82,5 @@ namespace PhEngine.ThaiTMP
             LexTo.Instance.TryInitialize();
             NotifyChange();
         }
-    }
-    
-    public enum TokenizationSeparator
-    {
-        ZWSP, SlashAndZWSP
     }
 }
